a6b5f3573947ad54982525c26b14dcd1e3710399
THEPEQUENO <cavalcanteitalogabriel19@gmail.com>
2023-11-02 16:06:07 -0300

    samsungo

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import json
 
 def load_json(path):
     try:
-        with open(path, "r") as file:
+        with open(path, "r", encoding='utf-8') as file:
             data = json.load(file)
             return data
     except Exception as e:
