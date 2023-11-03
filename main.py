99e07545646970f606da54fd46213c3f75120662
Pablo Pereira <vinicius7pablo@gmail.com>
2023-11-03 03:49:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 import PyQt5.QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
-from PyQt5.QtCore import QObject, QEvent
-from PyQt5.QtGui import QKeyEvent
 from random import randint
 
 from variables import (dicas_path)
@@ -72,7 +70,7 @@ class MainWindow(QMainWindow):
             ui_dica.dica2.setVisible(True)
         elif ui_dica.reveladas == 2:
             ui_dica.dica3.setVisible(True)
-            ui_dica.pushButton.setText('Próxima')
+            ui_dica.pushButton.setText('Explicação')
         elif ui_dica.reveladas == 3:
             self.pag_index += 1
             self.stackedWidget.setCurrentIndex(self.pag_index)
@@ -85,39 +83,6 @@ class MainWindow(QMainWindow):
         else:
             print("Resposta errada!")
             print(ui_dica.dica['resposta'])
-
-'''
-class MainWindow(QMainWindow, Ui_Jogo):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)
- 
-        self.dicas = load_json(dicas_path)
-    	
-        self.titulo.setText('3 Pistas - Filosofia')
-        self.dica2.setHidden(True)
-        self.dica3.setHidden(True)
-
-        self.reveladas = 0
-
-        self.pushButton.clicked.connect(self.exibirDica)
-        self.sendButton.clicked.connect(self.sendAnswer)
-        self.input_resposta.returnPressed.connect(self.sendAnswer)
-
-    def exibirDica(self):
-        self.reveladas += 1
-        if self.reveladas == 1:
-            self.dica2.setVisible(True)
-        
-        elif self.reveladas == 2:
-            self.dica3.setVisible(True)
-            self.pushButton.setText('Próxima')
-            makeMsgBox(self)           
- 
-    def sendAnswer(self):
-        text = str(self.input_resposta.text())
-        print(text)
-'''
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
